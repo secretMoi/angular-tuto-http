991adb1db3b48ea585f412cba3e5a73ca54b3d49
secretMoi <win.moi@hotmail.com>
2022-02-04 15:09:40 +0100

    Using a Service for Http Requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {map} from "rxjs/operators";
 import {Post} from "./post.model";
+import {PostService} from "./post.service";
 
 @Component({
   selector: 'app-root',
@@ -12,49 +11,23 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching: boolean = false;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private postService: PostService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.postService.fetchPosts();
   }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    this.httpClient.post<{ name: string }>(
-      'https://angular-tuto-http-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
-      postData
-    ).subscribe(responseData => {
-      console.log(responseData);
-    });
+    this.postService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
     // Send Http request
-    this.fetchPosts();
+    this.postService.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
-  }
-
-  private fetchPosts() {
-    this.isFetching = true;
-    this.httpClient.get<{ [key: string]: Post }>(
-      'https://angular-tuto-http-default-rtdb.europe-west1.firebasedatabase.app/posts.json'
-    ).pipe(map(responseData => {
-      const postArray: Post[] = []; // transforme la réponse en tableau
-      for(const key in responseData) {
-        if(responseData.hasOwnProperty(key)) {
-          // pour chaque élément, crée un objet avec les mêmes données (clé/valeur) + rajoute l'id unique
-          postArray.push({ ...responseData[key], id: key });
-        }
-      }
-
-      return postArray;
-    }))
-      .subscribe(posts => {
-        this.isFetching = false;
-        this.loadedPosts = posts;
-    });
   }
 }
