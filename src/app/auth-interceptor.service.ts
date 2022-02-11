4a423ae8df5589d6c2842df121ea7ea78ca09562
quentin.bolsee <quentin.bolsee@lampiris.be>
2022-02-11 10:33:13 +0100

    Manipulating Request Objects

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -6,7 +6,10 @@ import {Observable} from "rxjs";
 export class AuthInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Request intercepted');
-    return next.handle(req);
+
+    const modifiedRequest = req.clone({headers : req.headers.append('Auth', 'xyz')}); // clone car la req est immutable
+
+    return next.handle(modifiedRequest);
   }
 
 }
