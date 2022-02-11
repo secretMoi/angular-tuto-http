9b478f4eef58e2c8c475963b1435908ae6f88962
secretMoi <win.moi@hotmail.com>
2022-02-11 10:42:14 +0100

    Multiple Interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import {AuthInterceptorService} from "./auth-interceptor.service";
+import {LoggingInterceptorService} from "./logging-interceptor.service";
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,10 +14,15 @@ import {AuthInterceptorService} from "./auth-interceptor.service";
     FormsModule,
     HttpClientModule
   ],
-  providers: [
+  providers: [ // l'ordre défini est important
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptorService,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoggingInterceptorService,
       multi: true
     }
   ],
